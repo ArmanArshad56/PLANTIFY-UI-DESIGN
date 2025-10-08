3f6f70b6178ba7281761ffcfa236f3eb1d0d7b7f
ArmanArshad56 <armanarshad517@gmail.com>
2025-10-08 06:30:43 +0500

    Fix minor formatting issues in MyApp and LoginScreen classes

--- lib/main.dart
+++ lib/main.dart
@@ -104,6 +104,7 @@ ThemeData _buildTheme() {
 /// ------------------------------------------------------------
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
